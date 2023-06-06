762e98b29df324f50f0e31c806d6a017aa12592a
Adelicia <a.seq2804@gmail.com>
2023-06-06 08:32:20 +0530

    part 1.5, refactored code

--- part1/course-info/src/App.js
+++ part1/course-info/src/App.js
@@ -1,7 +1,53 @@
-import Header from "./Header";
-import Content from "./Content";
-import Total from "./Total";
-import Desc from "./Desc";
+const Header = (props) => {
+  console.log(props);
+  return (
+    <>
+      <h1 style={{color:'#4C2878', fontSize:'3rem'}}>{props.title.name}</h1>
+    </>
+  );
+};
+
+const Part = (props) => {
+  console.log(props);
+  return (
+    <>
+      <p style={{color:'#BE2260', fontSize:'1.5rem'}}>
+        {props.names}: {props.numbers}
+      </p>
+    </>
+  );
+};
+
+
+const Content = (props) => {
+  console.log(props);
+  const array = props.partsArr.parts;
+  return (
+    <>
+      <Part names={array[0].name} numbers={array[0].exercises} />
+      <Part names={array[1].name} numbers={array[1].exercises} />
+      <Part names={array[2].name} numbers={array[2].exercises} />
+    </>
+  );
+};
+
+const Total = (props) => {
+  console.log(props);
+  const array = props.exerciseArr.parts 
+  return (
+    <>
+      <p style={{color:'#BE2260', fontSize:'1.5rem'}}>Number of exercises: {array[0].exercises+array[1].exercises+array[2].exercises}</p>
+    </>
+  );
+};
+
+const Desc = () => {
+  return (
+      <>
+          <p style={{color:'#1198DA', fontSize:'1rem'}}>[This app was an exercise to understand and learn how props work. :{')'}]</p>
+      </>
+  )
+}
 
 const App = () => {
   const course = {
