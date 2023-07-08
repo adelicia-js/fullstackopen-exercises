c73e5f965ef89375847f8db4a1078d8524b2f098
Adelicia <a.seq2804@gmail.com>
2023-07-08 13:55:39 +0530

    redid part 1.2, but using Vite + React (for part2)

--- part2/course-info/src/App.jsx
+++ part2/course-info/src/App.jsx
@@ -1,61 +1,87 @@
-import './App.css'
+import "./App.css";
 import PropTypes from "prop-types";
 
-const Header = ({course}) => {
+const Header = ({ course }) => {
   return (
     <>
       <h1>{course}</h1>
     </>
-  )
-}
+  );
+};
 
 Header.propTypes = {
   course: PropTypes.string,
-} 
+};
 
-const Content = ({part, exercises}) => {
+const Part = ({ part, exercises }) => {
   return (
     <>
-      <p>{part} {exercises}</p>
+      <p>
+        {part} {exercises}
+      </p>
     </>
-  )
-}
+  );
+};
 
-Content.propTypes = {
+Part.propTypes = {
   part: PropTypes.string,
   exercises: PropTypes.number,
-}
+};
 
-const Total = ({sum}) => {
+const Content = (props) => {
+  return (
+    <>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
+    </>
+  );
+};
+
+Content.propTypes = {
+  part1: PropTypes.string,
+  part2: PropTypes.string,
+  part3: PropTypes.string,
+  exercises1: PropTypes.number,
+  exercises2: PropTypes.number,
+  exercises3: PropTypes.number,
+};
+
+const Total = ({ sum }) => {
   return (
     <>
       <p>Number of exercises {sum}</p>
     </>
-  )
-}
+  );
+};
 
 Total.propTypes = {
   sum: PropTypes.number,
-}
+};
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
 
   return (
     <div>
-      <Header course={course}/>
-      <Content part={part1} exercises={exercises1}/>
-      <Content part={part2} exercises={exercises2}/>
-      <Content part={part3} exercises={exercises3}/>
-      <Total sum={exercises1+exercises2+exercises3}/>
+      <Header course={course} />
+      <Content
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
+      />
+      <Total sum={exercises1 + exercises2 + exercises3} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
