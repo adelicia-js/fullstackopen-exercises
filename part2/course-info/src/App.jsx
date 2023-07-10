4bd49ebb55a665a90933e47e30cb7a6cf62bc857
Adelicia <a.seq2804@gmail.com>
2023-07-10 23:20:13 +0530

    part 2.1

--- part2/course-info/src/App.jsx
+++ part2/course-info/src/App.jsx
@@ -1,9 +1,9 @@
 import PropTypes from "prop-types";
 
-const Header = ({courseArr}) => {
+const Header = ({ courseArr }) => {
   return (
     <>
-      <h1 style={{color:'#4C2878', fontSize:'3rem'}}>{courseArr.name}</h1>
+      <h1 style={{ color: "#4C2878", fontSize: "3rem" }}>{courseArr.name}</h1>
     </>
   );
 };
@@ -16,7 +16,7 @@ Header.propTypes = {
 const Part = ({ part, exercises }) => {
   return (
     <>
-      <p style={{color:'#BE2260', fontSize:'1.5rem'}}>
+      <p style={{ color: "#BE2260", fontSize: "1.5rem" }}>
         {part}: {exercises}
       </p>
     </>
@@ -31,9 +31,18 @@ Part.propTypes = {
 const Content = ({ courseArr }) => {
   return (
     <>
-      <Part part={courseArr.parts[0].name} exercises={courseArr.parts[0].exercises} />
-      <Part part={courseArr.parts[1].name} exercises={courseArr.parts[1].exercises} />
-      <Part part={courseArr.parts[2].name} exercises={courseArr.parts[2].exercises} />
+      <Part
+        part={courseArr.parts[0].name}
+        exercises={courseArr.parts[0].exercises}
+      />
+      <Part
+        part={courseArr.parts[1].name}
+        exercises={courseArr.parts[1].exercises}
+      />
+      <Part
+        part={courseArr.parts[2].name}
+        exercises={courseArr.parts[2].exercises}
+      />
     </>
   );
 };
@@ -48,8 +57,8 @@ Content.propTypes = {
 const Total = ({ courseArr }) => {
   return (
     <>
-      <p style={{color:'#BE2260', fontSize:'1.5rem'}}>
-        Number of exercises: {""}
+      <p style={{ color: "#BE2260", fontSize: "1.5rem" }}>
+        Total of exercises: {""}
         {courseArr.parts[0].exercises +
           courseArr.parts[1].exercises +
           courseArr.parts[2].exercises}
@@ -66,37 +75,59 @@ Total.propTypes = {
 
 const Desc = () => {
   return (
-      <>
-          <p style={{color:'#1198DA', fontSize:'1rem'}}>[This app was an exercise to understand and learn how props work. :{')'}]</p>
-      </>
-  )
-}
+    <>
+      <p style={{ color: "#1198DA", fontSize: "1rem" }}>
+        This app was an exercise to understand and learn:
+        <ul>
+          <li>how props work</li>
+          <li>how to render collections</li>
+        </ul>
+      </p>
+    </>
+  );
+};
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header courseArr={course} />
+      <Content courseArr={course} />
+      {/* <Total courseArr={course}/> */}
+      <Desc />
+    </div>
+  );
+};
+
+Course.propTypes = {
+  course: PropTypes.object,
+};
 
 const App = () => {
   const course = {
-    name: 'Half Stack application development',
+    id: 1,
+    name: "Half Stack application development",
     parts: [
       {
-        name: 'Fundamentals of React',
-        exercises: 10
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
       },
       {
-        name: 'Using props to pass data',
-        exercises: 7
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
       },
       {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
 
   return (
-    <div style={{fontFamily:'monospace', backgroundColor:'#E6EE9C'}}>
-      <Header courseArr={course} />
-      <Content courseArr={course} />
-      <Total courseArr={course}/>
-      <Desc/>
+    <div style={{ fontFamily: "monospace", backgroundColor: "#E6EE9C" }}>
+      <Course course={course} />
     </div>
   );
 };
